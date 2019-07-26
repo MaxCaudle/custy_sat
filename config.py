d30709a959341f99f6230fedfc5836b733524d33
Mason <maxwell.caudle@gmail.com>
2019-07-26 12:55:51 -0600

    forgot this file

---
+++ config.py
@@ -0,0 +1,3 @@
+# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+
+DATABASE_URI = 'postgres+psycopg2://postgres:purplerain7@localhost:5432/custy_satisfaction'
