c8c52e67114bed0ee2c16bf86b273468bf9b7824
Mason <maxwell.caudle@gmail.com>
2019-08-01 14:33:44 -0600

    have a drop down working now. I want tomove the data object to a global variable even though it's not recommended because I'll update the global variable but only pull data as a copy.

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,4 @@
-from dashboard_helpers import get_df_from_sql, weekly_bar_chart
-
+from dashboard_helpers import Data, get_locations
 import datetime
 
 import dash
@@ -10,11 +9,18 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+locations = get_locations()
+
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div(
     html.Div([
         html.H4('Customer Satisfaction Dashboard'),
+        dcc.Dropdown(id='location_selector',
+                     options=[{'label': location, 'value': location} for location in locations]
+                             + [{'label':'All Locations', 'value':'all'}],
+                     value='all'
+                     ),
         dcc.Graph(id='live-update-graph'),
         dcc.Interval(id='interval-component',
                      interval=60*1000, #in milliseconds
@@ -23,11 +29,10 @@ app.layout = html.Div(
 )
 
 @app.callback(Output('live-update-graph', 'figure'),
-              [Input('interval-component', 'n_intervals')])
-def update_graph_live(n):
-    df = get_df_from_sql()
-    df_weekly = weekly_bar_chart(df)
-
+              [Input('interval-component', 'n_intervals'),
+               Input('location_selector', 'value')])
+def update_graph_live(n, location):
+    data = Data(locations)
     # Create the graph with subplots
     fig = subplots(rows=1, cols=1, vertical_spacing=0.2)
     fig['layout']['margin'] = {
@@ -35,7 +40,7 @@ def update_graph_live(n):
     }
     fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
 
-
+    df_weekly = data.df_week[location]
     fig.append_trace(go.Bar(name='Bad', x=df_weekly.index, y=df_weekly['1']), 1, 1)
     fig.append_trace(go.Bar(name='Fair', x=df_weekly.index, y=df_weekly['2']), 1, 1)
     fig.append_trace(go.Bar(name='Good', x=df_weekly.index, y=df_weekly['3']), 1, 1)
--- dashboard_helpers.py
+++ dashboard_helpers.py
@@ -1,28 +1,52 @@
 from helpers import get_session
 import numpy as np # is any good data project complete without at least one call to np?
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from datetime import datetime, timedelta
-sns.set()
-
-SQL_Query = \
-"""
-SELECT * FROM ratings
-"""
 
 
-def get_df_from_sql():
+def get_locations():
+    SQL_Query = \
+        """
+        SELECT DISTINCT device FROM ratings;
+        """
     engine, Session = get_session()
-    df = pd.read_sql_query(SQL_Query, con=engine)
-    return df
+    locations = pd.read_sql_query(SQL_Query, con=engine)['device'].tolist()
+    return locations
 
-def weekly_bar_chart(df):
-    df_week = df[df['date'].dt.date > datetime.date(datetime.now()) - timedelta(7)]
-    week_1 = df_week[df_week['rating'] == 1].groupby(df_week.date.dt.date).count()['id']
-    week_2 = df_week[df_week['rating'] == 2].groupby(df_week.date.dt.date).count()['id']
-    week_3 = df_week[df_week['rating'] == 3].groupby(df_week.date.dt.date).count()['id']
-    df_week = pd.concat([week_1, week_2, week_3], axis=1, sort=True).fillna(0)
-    df_week.columns = ['1', '2', '3']
 
-    return df_week
+class Data:
+    def __init__(self, locations):
+        self.SQL_Query = \
+            """
+            SELECT * FROM ratings;
+            """
+
+        self.df = self.get_df_from_sql()
+        self.locations = locations
+        self.df_week = self.make_all_weeklys()
+
+
+
+    def get_df_from_sql(self):
+        engine, Session = get_session()
+        df = pd.read_sql_query(self.SQL_Query, con=engine)
+        print('pulling data')
+        return df
+
+    def weekly_bar_chart(self, sub_df):
+        df_week = sub_df[sub_df['date'].dt.date > datetime.date(datetime.now())
+                                                    - timedelta(7)]
+        week_1 = df_week[df_week['rating'] == 1].groupby(df_week.date.dt.date).count()['id']
+        week_2 = df_week[df_week['rating'] == 2].groupby(df_week.date.dt.date).count()['id']
+        week_3 = df_week[df_week['rating'] == 3].groupby(df_week.date.dt.date).count()['id']
+        df_week = pd.concat([week_1, week_2, week_3], axis=1, sort=True).fillna(0)
+        df_week.columns = ['1', '2', '3']
+
+        return df_week
+
+    def make_all_weeklys(self):
+        weekly_dict = {'all': self.weekly_bar_chart(self.df)}
+        for location in self.locations:
+            weekly_dict[location] = self.weekly_bar_chart(self.df[self.df['device'] == location])
+
+        return weekly_dict
