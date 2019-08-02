ffe5119dfb1c7d6d99c187c66174a3cac93b1cd2
Mason <maxwell.caudle@gmail.com>
2019-08-02 15:09:14 -0600

    thats really annoying, i wish you didnt have to update the trace after adding it. seems like there should be a better way

--- dashboard_graphs.py
+++ dashboard_graphs.py
@@ -11,23 +11,31 @@ from dash.dependencies import Input, Output
 def past_7_days(data, location, fig):
     df_weekly = data.df_week[location]
     ones = go.Bar(name='Bad', x=df_weekly.index, y=df_weekly['1'],
-                            marker={'color': calc_color(1)})
+                            marker={'color': calc_color(1)},
+                  yaxis='y')
 
     average = go.Scatter(x=df_weekly.index.values.tolist(),
                          y=df_weekly['mean'].values.tolist(),
-                         yaxis='y3')
+                         yaxis='y1')
 
 
     twos = go.Bar(name='Fair', x=df_weekly.index, y=df_weekly['2'],
-                            marker={'color': calc_color(2)})
+                            marker={'color': calc_color(2)},
+                  yaxis='y')
 
     threes = go.Bar(name='Good', x=df_weekly.index, y=df_weekly['3'],
-                            marker={'color': calc_color(3)})
+                            marker={'color': calc_color(3)},
+                  yaxis='y')
 
     fig.append_trace(ones, 1, 1)
     fig.append_trace(twos, 1, 1)
     fig.append_trace(threes, 1, 1)
     fig.append_trace(average, 1, 1)
-    print(fig.layout)
-    #fig.layout.update(y3 = {'range':[0,3]})
+    
+    fig['data'][-1].update(yaxis='y2')
+    fig.layout.update(yaxis2 = {'range':[0,3],
+                                'side': 'right',
+                                'overlaying': 'y',
+                                'anchor': 'x'})
+    #print(fig.layout)
     return fig
