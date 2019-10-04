1d48afd6e49cfb5796a2fb02c07822b92488654e
Mason <maxwell.caudle@gmail.com>
2019-10-04 14:58:53 -0600

    hands

--- rating_app/app.py
+++ rating_app/app.py
@@ -23,24 +23,6 @@ def buttons():
         add_rating(Session, rating, location=request.args.get('location'))
     return render_template('survey.html', location=request.args.get('location'))
 
-# @app.route('/background_add_1')
-# def background_add_1():
-#     location = request.args.get('location')
-#     print(f"calling add 1 with location: {location}")
-#     add_rating(Session, 1, location=location)
-#     return redirect(url_for('.buttons', location=request.form.get('location')))
-#
-# @app.route('/background_add_2')
-# def background_add_2():
-#     print("calling add 2")
-#     add_rating(Session, 2)
-#     return "nothing"
-#
-# @app.route('/background_add_3')
-# def background_add_3():
-#     print("calling add 3")
-#     add_rating(Session, 3)
-#     return "nothing"
 
 if __name__ == "__main__":
     app.run()
